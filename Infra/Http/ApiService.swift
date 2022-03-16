2049e777d457ba52517b87715c0a6c4ac284d13d
rafaelnunesr <rafaelrafssk@hotmail.com>
2022-03-16 13:21:07 -0300

    add ApiService

---
+++ Infra/Http/ApiService.swift
@@ -0,0 +1,13 @@
+//
+//  ApiService.swift
+//  Infra
+//
+//  Created by Rafael Nunes Rios on 3/16/22.
+//
+
+import Foundation
+import RxSwift
+
+public class ApiService {
+    
+}
---
+++ InfraTests/Http/ApiServiceTests.swift
@@ -0,0 +1,12 @@
+//
+//  ApiServiceTests.swift
+//  InfraTests
+//
+//  Created by Rafael Nunes Rios on 3/16/22.
+//
+
+import XCTest
+
+class ApiServiceTests: XCTestCase {
+}
+
